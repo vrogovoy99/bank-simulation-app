1ace77811584eba8ac45a5e5705a0c38b7ab421e
Human <vladislav.rogovoy@gmail.com>
2024-03-15 18:26:50 -0400

    initial project

--- src/main/java/com/cydeo/repository/AccountRepository.java
+++ src/main/java/com/cydeo/repository/AccountRepository.java
@@ -2,16 +2,18 @@ package com.cydeo.repository;
 
 import com.cydeo.model.Account;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AccountRepository {
-public Account save(Account account){
-    System.out.println("Account saved");
-    return account;
-}
+    public static List<Account> accountList = new ArrayList<>();
+    public Account save(Account account){
+        accountList.add(account);
+        return account;
+    }
 
-public List<Account> findAll(){
-    return null;
-}
+    public List<Account> findAll(){
+        return accountList;
+    }
 
 }
